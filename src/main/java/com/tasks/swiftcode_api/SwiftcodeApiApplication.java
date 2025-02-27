85ef3541d7a4c06f4e1e6f9a3d6df6ec02f7bac1
gruszkinn951 <piotr.kupis777@gmail.com>
2025-02-28 00:23:36 +0100

    AppConfig
    App and Git first config

---
+++ src/main/java/com/tasks/swiftcode_api/SwiftcodeApiApplication.java
@@ -0,0 +1,13 @@
+package com.tasks.swiftcode_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftcodeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftcodeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tasks/swiftcode_api/SwiftcodeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasks.swiftcode_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftcodeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
