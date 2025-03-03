15735f7278417e60c1c5c60ae4b22ba7a52ffc54
gruszkinn951 <piotr.kupis777@gmail.com>
2025-03-03 22:05:36 +0100

    Endpoints
    - getMapping /country/{countryISO2code}
    - deleteMapping /{swift-code}

--- src/main/java/com/tasks/swiftcode_api/repositories/SwiftCodeRepository.java
+++ src/main/java/com/tasks/swiftcode_api/repositories/SwiftCodeRepository.java
@@ -4,6 +4,11 @@ import com.tasks.swiftcode_api.models.BankEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface SwiftCodeRepository extends JpaRepository<BankEntity, String> {
+    Optional<BankEntity> findFirstByCountryISO2(String countryISO2);
+    List<BankEntity> findAllBanksByCountryISO2(String countryISO2);
 }
